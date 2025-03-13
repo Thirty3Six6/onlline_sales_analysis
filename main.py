664af1d9843168f9cbd6fc945ae0e3a3561f914c
Karlo Radovan <karloogloc@gmail.com>
2025-03-13 13:07:50 +0100

    Dodani importovi ako je potrebno

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 if __name__ == "__main__":
     manager = ProductManager()
-    manager.add_products(Product("Laptop", 100, 5))
-    manager.add_products(Product("Mouse", 10, 20))
-    manager.show_all_product()
-    manager.total_value()
-    
-    cart= ShoppingCart()
-    
-    cart.add_to_cart(Product("Laptop", 100, 5), 2)
-    cart.add_to_cart(Product("Mouse", 10, 20), 5)
-    cart.add_to_cart(Product("Laptop", 100, 5), 3)
-    cart.show_cart()
-    
-    cart.calculate_total()
+    manager.add_products(Product("Gaming Laptop", 100, 10))
+    manager.add_products(Product("Gaming mouse", 20, 50))
+
+import product_manager
+import product
+from product import Product
+from product_manager import ProductManager
+
+##
